86c9faccb9bfa7bedf3a98840fb0926ef8c98934
rfgon <rfgoncalvesmail@gmail.com>
2022-04-26 09:48:45 +0100

    feat: Initial twosum() framework

--- 1-Two-Sum/cpp/include/utils.h
+++ 1-Two-Sum/cpp/include/utils.h
@@ -1,4 +1,21 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <iostream>
+#include <vector>
+
+namespace utils
+{
+
+    class Solution {
+        public:
+            std::vector<int> twoSum(std::vector<int>& nums, int target) {
+                std::vector<int> solution(2);
+
+                return solution;
+            }
+    };
+
+} // namespace utils
+
 #endif // UTILS_H
