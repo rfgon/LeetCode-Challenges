ee361fe7344c1b142951e6df5dd42aeb27e02352
rfgon <rfgoncalvesmail@gmail.com>
2022-04-26 14:53:41 +0100

    feat: Add solution logic and test it

--- 1-Two-Sum/cpp/include/utils.h
+++ 1-Two-Sum/cpp/include/utils.h
@@ -9,16 +9,36 @@ namespace utils
 {
 
     // Solution to the challenge
-    class Solution {
+    class Solution
+    {
         public:
-            std::vector<int> twoSum(std::vector<int>& nums, int target) {
+            std::vector<int> twoSum(std::vector<int>& nums, int target)
+            {
                 std::vector<int> solution(2);
+
+                bool found_indices = false;
+
+                // Try every combination of numbers pairs
+                for (int i = 0; i < (int)nums.size(); ++i) {
+                    for (int j = i + 1; j < (int)nums.size(); ++j) {
+                        if (nums.at(i) + nums.at(j) == target) {
+                            solution.at(0) = i;
+                            solution.at(1) = j;
+                            found_indices = true;
+                            break;
+                        }
+                    }
+
+                    if (found_indices) {
+                        break;
+                    }
+                }
 
                 return solution;
             }
     };
 
-    // Test case function
+    // Test case
     void testCase(const std::vector<int>& output_1, const std::vector<int>& output_2)
     {
         if (std::equal(output_1.begin(), output_1.end(), output_2.begin())) {
--- 1-Two-Sum/cpp/src/main.cpp
+++ 1-Two-Sum/cpp/src/main.cpp
@@ -4,21 +4,29 @@ using namespace utils;
 
 int main()
 {
+    Solution solution;
+
     // Test cases
     // Test 1
     std::vector<int> nums = {2, 7, 11, 15};
     int target = 9;
     std::vector<int> output = {0, 1};
 
+    testCase(output, solution.twoSum(nums, target));
+
     // Test 2
     nums = {3, 2, 4};
     target = 6;
     output = {1, 2};
 
+    testCase(output, solution.twoSum(nums, target));
+
     // Test 3
     nums = {3, 3};
     target = 6;
     output = {0, 1};
+
+    testCase(output, solution.twoSum(nums, target));
 
     return 0;
 }
